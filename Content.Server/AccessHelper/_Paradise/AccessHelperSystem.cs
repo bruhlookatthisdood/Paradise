5ff3dde75f3d072255fcb1801ba7cfee725a1fa7
The Malf E <spessengineer1@gmail.com>
2026-07-31 08:33:52 -0500

    start system

--- Content.Server/AccessHelper/_Paradise/AccessHelperSystem.cs
+++ Content.Server/AccessHelper/_Paradise/AccessHelperSystem.cs
@@ -1,7 +1,11 @@
 ﻿namespace Content.Server.AccessHelper._Paradise
 {
-    public sealed partial class AccessHelperSystem : EntitySystem
+    public sealed class AccessHelperSystem : EntitySystem
     {
-
+        public override void Initialize()
+        {
+            base.Initialize();
+            SubscribeLocalEvent<AccessHelperComponent, MapInitEvent>(OnMapInit);
+        }
     }
 }
