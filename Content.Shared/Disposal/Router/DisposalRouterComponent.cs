008ccea515c223180537245baf53239f46586e5b
The Malf E <spessengineer1@gmail.com>
2026-08-02 12:52:06 -0500

    atomized mail sorting helpers

--- Content.Shared/Disposal/Router/DisposalRouterComponent.cs
+++ Content.Shared/Disposal/Router/DisposalRouterComponent.cs
@@ -1,7 +1,6 @@
 using Content.Shared.Disposal.Router;
 using Robust.Shared.Audio;
 using Robust.Shared.GameStates;
-using Robust.Shared.Graphics.RSI;
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Disposal.Components;
