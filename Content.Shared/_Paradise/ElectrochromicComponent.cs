4dfccd3c93d4d69fa8824d82a7ee478a61117079
The Malf E <spessengineer1@gmail.com>
2026-08-05 10:07:50 -0500

    well its something

---
+++ Content.Shared/_Paradise/ElectrochromicComponent.cs
@@ -0,0 +1,9 @@
+﻿namespace Content.Shared._Paradise;
+
+[RegisterComponent, AutoGenerateComponentState]
+public sealed partial class ElectrochromicComponent : Component
+{
+    [DataField, AutoNetworkedField]
+    public bool IsOpaque = false;
+
+}
---
+++ Content.Shared/_Paradise/ElectrochromicSystem.cs
@@ -0,0 +1,16 @@
+﻿namespace Content.Shared._Paradise;
+
+public sealed class ElectrochromicSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ElectrochromicComponent, ComponentInit>(OnInit);
+    }
+
+    private void OnInit(EntityUid uid, ElectrochromicComponent component, ComponentInit args)
+    {
+
+    }
+}
