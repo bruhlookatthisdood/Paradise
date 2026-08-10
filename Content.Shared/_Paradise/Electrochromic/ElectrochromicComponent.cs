327659110241d4c755421eb79c9a5a8acb3e32cc
The Malf E <spessengineer1@gmail.com>
2026-08-10 15:39:43 -0500

    functionality

--- Content.Shared/_Paradise/Electrochromic/ElectrochromicComponent.cs
+++ Content.Shared/_Paradise/Electrochromic/ElectrochromicComponent.cs
@@ -1,9 +1,11 @@
-﻿namespace Content.Shared._Paradise;
+﻿using Content.Shared.DeviceLinking;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared._Paradise.Electrochromic;
 
 [RegisterComponent, AutoGenerateComponentState]
 public sealed partial class ElectrochromicComponent : Component
 {
     [DataField, AutoNetworkedField]
     public bool IsOpaque = false;
-
 }
---
+++ Content.Shared/_Paradise/Electrochromic/ElectrochromicSystem.cs
@@ -0,0 +1,33 @@
+﻿using Content.Shared.DeviceLinking;
+using Content.Shared.DeviceLinking.Events;
+using Content.Shared.DeviceNetwork.Events;
+
+namespace Content.Shared._Paradise.Electrochromic;
+
+public sealed partial class ElectrochromicSystem : EntitySystem
+{
+    [Dependency] private OccluderSystem _occluderSystem = default!;
+    [Dependency] private SharedDeviceLinkSystem _deviceLinkSystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ElectrochromicComponent, SignalReceivedEvent>(OnSignalReceived);
+    }
+
+    private void OnSignalReceived(Entity<ElectrochromicComponent> entity, ref SignalReceivedEvent args)
+    {
+        Log.Warning("SignalRecieved!!!!!!!!");
+
+        if(!TryComp<OccluderComponent>(entity, out var occluder))
+            return;
+
+        // Flip our IsOpaque value
+        entity.Comp.IsOpaque = !entity.Comp.IsOpaque;
+        _occluderSystem.SetEnabled(entity.Owner, entity.Comp.IsOpaque, occluder);
+        Dirty(entity);
+    }
+
+
+}
